4e90a2e1c13b51aa68bb0d2b11627d1666ddca01
CocolinoFan <arvamircea@gmail.com>
2024-06-17 15:39:55 +0100

    A nothing burger, lesson 4 and 5, codeing practice.

--- scenes/Icon.gd
+++ scenes/Icon.gd
@@ -5,7 +5,7 @@ var fish = 5
 var player_name = "robot"
 var hearts = 3.5
 const SPEED = 2
-
+var is_godot_awesome = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -13,4 +13,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(deg_to_rad(SPEED))
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED))
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED))
+
+func minion():
+	print("Bana!")
+
+func add_these_numbers(x, y):
+	var sun = x + y
+	return sun
